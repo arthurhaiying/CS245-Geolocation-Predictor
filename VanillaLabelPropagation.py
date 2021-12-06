ee7a4dda684c12a85dbcdd0860ec1901c2053203
lc <lc4324@github.com>
2021-12-05 16:21:12 -0800

    Implement Oracle label propagation

--- VanillaLabelPropagation.py
+++ VanillaLabelPropagation.py
@@ -5,6 +5,7 @@ Created on Sun Nov 28 15:32:36 2021
 @author: luche
 """
 import numpy as np
+from sklearn.preprocessing import normalize
 
 def computer_transition_matrix(W):
   """
@@ -15,10 +16,10 @@ def computer_transition_matrix(W):
   W: nxn weight matrix, where W_ij measures the similarity between
     node i and node j
   """
+  return normalize(W, axis=1, norm='l1')
+  #return np.nan_to_num(W/np.sum(W,axis=1,keepdims=True))
 
-  return W/np.sum(W,axis=0)
-
-def vanilla_label_propagation(Y, T, mask, max_iter):
+def vanilla_label_propagation(Y, T, mask, max_iter, ground_truth, num_masked_out):
   """
   Do vanilla label propagation until convergence. Return the 
   new label distribution
@@ -43,45 +44,74 @@ def vanilla_label_propagation(Y, T, mask, max_iter):
     # propagate
     Y_cur = np.matmul(T, Y_cur)
     # row-normalize
-    Y_cur = Y_cur/np.sum(Y_cur,axis=1,keepdims=True)
-    Y_cur = np.nan_to_num(Y_cur)
+    Y_cur = normalize(Y_cur, axis=1, norm='l1')
+    #Y_cur = Y_cur/np.sum(Y_cur,axis=1,keepdims=True)
+    #Y_cur = np.nan_to_num(Y_cur)
     # clamp the labeled data
     Y_cur = np.matmul(mask, Y_cur) + Y-np.matmul(mask, Y)
     #print(Y_cur)
     # increase the counter
     counter += 1
-  
-  print("counter: ", counter)
+    print("iteration: ", counter)
+    
+    # temporary
+    num_correct = num_masked_out - np.count_nonzero(np.argmax(ground_truth, axis = 1)- np.argmax(Y_cur, axis = 1))
+    print("num_correct: ", num_correct)          
+    print("accuray: ", num_correct/num_masked_out)
+    
   return Y_cur
 
 
+def oracle_label_propagation(ground_truth, T):
+    """
+    Assume we know the ground truth label of the neighbors when predicting 
+    the label for each node.
+    """
+    np.fill_diagonal(T,0)
+    predictions = np.matmul(T, ground_truth)
+    num_correct = ground_truth.shape[0] - np.count_nonzero(np.argmax(ground_truth, axis = 1)- np.argmax(predictions, axis = 1))
+    print("num_correct: ", num_correct)          
+    print("accuray: ", num_correct/ground_truth.shape[0])
+    return predictions
+
+
 def run_test_case():
-    W = np.array([[1,0.5,0.5,0,0,0],
-                  [0.5,1,0.5,0.4,0,0],
-                  [0.5,0.5,1,0,0,0],
-                  [0,0.4,0,1,0.5,0.5],
-                  [0,0,0,0.5,1,0],
-                  [0,0,0,0.5,0,1]])
-    print("W: ", W)
+    W = np.array([[0,0.5,0.6,0,0,0],
+                  [0.5,0,0.5,0.4,0,0],
+                  [0.6,0.5,0,0,0,0],
+                  [0,0.4,0,0,0.5,0.5],
+                  [0,0,0,0.5,0,0],
+                  [0,0,0,0.5,0,0]])
+    print("W: ")
+    print(W)
     T = computer_transition_matrix(W)
     print("T: ", T)
     Y = np.array([[1,0,0],
-                  [1/3,1/3,1/3],
+                  [0,0,1],
                   [1/3,1/3,1/3],
                   [1/3,1/3,1/3],
                   [1/3,1/3,1/3],
                   [0,1,0],])
     print("Y: ", Y)
     mask = np.array([[0,0,0,0,0,0],
-                     [0,1,0,0,0,0],
+                     [0,0,0,0,0,0],
                      [0,0,1,0,0,0],
                      [0,0,0,1,0,0],
                      [0,0,0,0,1,0],
                      [0,0,0,0,0,0],])
     print("mask: ", mask)
     
-    print("results: ")
-    print(vanilla_label_propagation(Y, T, mask, 1000))
+    ground_truth = np.array([[1,0,0],
+                             [0,0,1],
+                             [1,0,0],
+                             [0,1,0],
+                             [0,1,0],
+                             [0,1,0],])
+    
+    print("vanilla label propagation")
+    print(vanilla_label_propagation(Y, T, mask, 10, ground_truth, 3))
+    print("oracle label propagation")
+    print(oracle_label_propagation(ground_truth, T))
 
 if __name__ == "__main__":
     run_test_case()
